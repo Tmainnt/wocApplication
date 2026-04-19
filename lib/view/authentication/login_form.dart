9d5d53679f5897929dd6da04385fc3327ca2f6a7
Tmainnt <gamerthann@gmail.com>
2026-04-19 10:09:05 +0700

    update

--- lib/view/authentication/login_form.dart
+++ lib/view/authentication/login_form.dart
@@ -10,6 +10,43 @@ class LoginForm extends StatefulWidget {
 class LogFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext contexct) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage(""), fit: BoxFit.cover),
+            ),
+          ),
+          Center(
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          text: "Pedometer &",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: "Workout",
+                              style: TextStyle(color: Colors.orange),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

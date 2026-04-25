4d59b4aecac873b11309b08d8b4a55586d03c5e4
Tmainnt <gamerthann@gmail.com>
2026-04-25 11:11:04 +0700

    update

--- lib/database/queryUser.go
+++ lib/database/queryUser.go
@@ -1,6 +1,15 @@
 package db
 
+import (
+	"database/sql"
+	"net/http"
+)
+
 type User struct {
 	ID   int    `json:"id"`
-	Name string `json:name`
+	Name string `json:"name"`
+}
+
+func loginHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {}
 }

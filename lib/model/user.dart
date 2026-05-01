d9022fba3a8f8886227dfd2fd16ae491b9de4881
Tmainnt <gamerthann@gmail.com>
2026-05-01 12:41:41 +0700

    delete enum replace with String type

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,24 +1,18 @@
-// ignore: constant_identifier_names
-enum Role { User, Admin }
-
-// ignore: constant_identifier_names
-enum Gender { Male, Female }
-
 class User {
   String _email;
   String _name;
-  Gender _gender;
+  String _gender;
   String _dof;
   String _phone;
-  Role _role;
+  String _role;
 
   User({
     required String email,
     required String name,
-    required Gender gender,
+    required String gender,
     required String dof,
     required String phone,
-    required Role role,
+    required String role,
   }) : _email = email,
        _name = name,
        _gender = gender,
@@ -32,19 +26,15 @@ class User {
       name: json["user_name"],
       dof: json["date_of_birth"],
       phone: json["phone_number"],
-      gender: Gender.values.firstWhere(
-        (e) => e.name.toLowerCase() == json["user_gender"].toLowerCase(),
-      ),
-      role: Role.values.firstWhere(
-        (e) => e.name.toLowerCase() == json["role"].toLowerCase(),
-      ),
+      gender: json["user_gender"],
+      role: json["role"],
     );
   }
 
   String get email => _email;
   String get name => _name;
-  Gender get gender => _gender;
+  String get gender => _gender;
   String get dof => _dof;
   String get phone => _phone;
-  Role get role => _role;
+  String get role => _role;
 }

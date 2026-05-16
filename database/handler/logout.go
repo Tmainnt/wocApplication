c7f7302f42898741ffad98aba2c8e159af89deb7
Tmainnt <gamerthann@gmail.com>
2026-05-16 14:13:40 +0700

    complete logout.go

--- database/handler/logout.go
+++ database/handler/logout.go
@@ -3,10 +3,29 @@ package handler
 import (
 	"database/sql"
 	"net/http"
+	"time"
+	auth "woc/database/auth"
 )
 
 func LogoutHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		
+		claims := r.Context().Value(auth.UserContextKey).(*auth.Claims)
+
+		db.Exec(`DELETE FROM refresh_token WHERE user_id_fk = $1`, claims.UserID)
+
+		db.Exec(`UPDATE users SET status = 'Offline' WHERE user_id = $1`, claims.UserID)
+
+		// Clear cookie
+		http.SetCookie(w, &http.Cookie{
+			Name:     "refresh_token",
+			Value:    "",
+			HttpOnly: true,
+			Expires:  time.Unix(0, 0),
+			MaxAge:   -1,
+			Path:     "/refresh",
+		})
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("logged out"))
 	}
 }

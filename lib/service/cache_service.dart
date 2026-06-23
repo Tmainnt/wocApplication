cf401cdc3ef7353a3e5486007bfa8927cd5bc60f
Tracy Magz <gamerthann@gmail.com>
2026-06-23 15:45:08 +0700

    change saveUserData method parameter from List<String> to Object class User

--- lib/service/cache_service.dart
+++ lib/service/cache_service.dart
@@ -1,17 +1,31 @@
 import "package:cache_storage/cache_storage.dart";
+import "package:woc/model/user.dart";
 
 class CacheService {
     final _cacheStorage  = CacheStorage.open();
     
-    void saveUserData(List<String> userData){
-        _cacheStorage.save(key: "user_data", value: userData);
-    }
-
     List<String> getUserData(){
         final userData = _cacheStorage.match(key: "user_data");
         if (userData.isNotEmpty) {
             return userData;
         }
         return [];
+    }
+
+    void saveUserData(User userData){
+        final userDataList = [
+            userData.token,
+            userData.email,
+            userData.name,
+            userData.gender,
+            userData.dob.toIso8601String(),
+            userData.phone,
+            userData.role,
+            userData.profileImage,
+            userData.status,
+            userData.createTimestamp.toIso8601String(),
+            userData.updateTimestamp.toIso8601String(),
+        ];
+        _cacheStorage.save(key: "user_data", value: userDataList);
     }
 }

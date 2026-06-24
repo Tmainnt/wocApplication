890adcd70c432c880626a8db43e131345bd9e364
Tracy Magz <gamerthann@gmail.com>
2026-06-24 12:52:48 +0700

    update check userData in cache

--- lib/service/cache_service.dart
+++ lib/service/cache_service.dart
@@ -1,19 +1,22 @@
 import "package:cache_storage/cache_storage.dart";
 import "package:woc/model/user.dart";
+import "package:woc/provider/user_provider.dart";
 
 class CacheService {
     final _cacheStorage  = CacheStorage.open();
     
-    List<String> getUserData(){
-        final userData = _cacheStorage.match(key: "user_data");
-        if (userData.isNotEmpty) {
-            return userData;
-        }
-        return [];
+    User getUserData(){
+        return _cacheStorage.match(key: "user_data");
     }
 
     void saveUserData(User userData){
 
         _cacheStorage.save(key: "user_data", value: userData);
+    }
+
+    void setUserProvider() {
+        UserProvider userProvider = UserProvider();
+        final userData = getUserData();
+        userProvider.setUser(userData);
     }
 }

065f9f9a490783ffb1d1392b2c9f0ead701c92ac
Tracy Magz <gamerthann@gmail.com>
2026-06-25 12:26:27 +0700

    move code to main_wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:woc/provider/user_provider.dart';
-import 'package:woc/service/token_service.dart';
-import 'package:woc/view/authentication/login_form.dart';
-import 'package:woc/view/home_page.dart';
-import 'package:woc/service/cache_service.dart';
+import 'package:woc/main_wrapper.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(create: (_) => UserProvider(), child: MyApp()));
@@ -15,21 +12,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    CacheService().setUserProvider();
-    return FutureBuilder(
-      future: TokenService.getAccessToken(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: HomePage(),
-          );
-        }
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: LoginForm(),
-        );
-      },
-    );
+    return MainWrapper();
   }
 }

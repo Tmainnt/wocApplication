5ed3fbc6e7aff00013271175237f34fcbd304b27
Tracy Magz <gamerthann@gmail.com>
2026-07-17 13:15:19 +0700

    fix bug

--- database/postDB/readPost.go
+++ database/postDB/readPost.go
@@ -4,14 +4,15 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
+	"fmt"
 	auth "woc/database/auth"
 )
 
 type Post struct {
 	PostID          int    `json:"post_id"`
 	UserID          string `json:"user_id"`
-	Content         string `json:"post_content"`
-	Image           string `json:"post_image"`
+	Content         string `json:"content"`
+	Image           string `json:"image_url"`
 	CreateTimestamp string `json:"create_timestamp"`
 	UpdateTimestamp string `json:"update_timestamp"`
 	PostVisibility  string `json:"post_visibility"`
@@ -23,9 +24,13 @@ type Post struct {
 
 func GetAllPost(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		row, err := db.Query(`SELECT post_id_pk, user_id_pk, post_content, post_image, create_timestamp, update_timestamp, post_status, like_count, comment_count FROM posts WHERE post_visibility = 'public'`)
+		row, err := db.Query(`SELECT post_id, user_id_fk, content, image_url, create_timestamp, update_timestamp, post_visibility, post_status, like_count, comment_count, report_count FROM posts WHERE post_visibility = 'public'`)
 		if err != nil {
+			// debug
+			fmt.Println(err)
+
 			http.Error(w, "Error fetch data", 500)
+			return
 		}
 
 		defer row.Close()
@@ -33,7 +38,7 @@ func GetAllPost(db *sql.DB) http.HandlerFunc {
 		var post []Post
 		for row.Next() {
 			var p Post
-			row.Scan(&p.PostID, &p.UserID, &p.Content, &p.Image, &p.CreateTimestamp, &p.UpdateTimestamp, &p.PostVisibility, &p.PostStatus, &p.LikeCount, &p.CommentCount)
+			row.Scan(&p.PostID, &p.UserID, &p.Content, &p.Image, &p.CreateTimestamp, &p.UpdateTimestamp, &p.PostVisibility, &p.PostStatus, &p.LikeCount, &p.CommentCount, &p.ReportCount)
 			post = append(post, p)
 		}
 		w.WriteHeader(http.StatusOK)
@@ -45,7 +50,7 @@ func GetMyPost(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		claims := r.Context().Value(auth.UserContextKey).(*auth.Claims)
 
-		row, err := db.Query(`SELECT post_id, user_id_pk, post_content, post_image, like_count, comment_count, report_count, create_timestamp, update_timestamp FROM post WHERE user_id_pk = $1`, claims.UserID)
+		row, err := db.Query(`SELECT post_id, user_id_fk, content, image_url, like_count, comment_count, report_count, create_timestamp, update_timestamp FROM post WHERE user_id_pk = $1`, claims.UserID)
 
 		if err != nil {
 			http.Error(w, "Error fetch data", 500)

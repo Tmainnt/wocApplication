fd776eafe183ec34d93e735fdab6b8076a753f16
Tracy Magz <gamerthann@gmail.com>
2026-07-19 23:36:10 +0700

    fix bug: add materialApp in build method because Widget tree order is incorrect scaffold from main_wrapper call for materialApp but missing then it output error

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainWrapper();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainWrapper(),
+    );
   }
 }

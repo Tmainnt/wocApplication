5f0f777e7d0a327d61fcebb43bd8027d9d3b9780
Tracy Magz <gamerthann@gmail.com>
2026-07-19 23:40:28 +0700

    initialize this file

---
+++ lib/view/exercise/select_exersice_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SelectExersicePage extends StatefulWidget {
+  const SelectExersicePage({super.key});
+
+  @override
+  State<SelectExersicePage> createState() => SelectExersicePageState();
+}
+
+class SelectExersicePageState extends State<SelectExersicePage> {
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}

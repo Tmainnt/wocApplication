1ecc644ed7bf5f0f252748cea859ceb7c6f3fa41
Tracy Magz <gamerthann@gmail.com>
2026-07-22 13:21:53 +0700

    initialize page

---
+++ lib/view/community/new_post.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewPost extends StatefulWidget {
+  const NewPost({super.key});
+
+  @override
+  State<NewPost> createState() => NewPostState();
+}
+
+class NewPostState extends State<NewPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

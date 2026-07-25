b02d07020bb38a0d940f749c6a509ba2899616e6
Tracy Magz <gamerthann@gmail.com>
2026-07-25 17:06:59 +0700

    remove expanded, replace Appbar with TopAppbar()

--- lib/view/community/new_post.dart
+++ lib/view/community/new_post.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:woc/theme/widget_color.dart';
+import 'package:woc/widget/appbar/top_appbar.dart';
 import 'package:woc/widget/community/widget_bottom_appbar.dart';
 
 class NewPost extends StatefulWidget {
@@ -18,7 +19,14 @@ class NewPostState extends State<NewPost> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: Icon(Icons.cancel, color: Colors.white,),),
+      appBar: TopAppbar(
+        centerText: '', 
+        leadingContent: Icon(Icons.cancel), 
+        trailingContent: ElevatedButton(
+          onPressed: (){}, 
+          child: Text("บันทึก"),
+        )
+      ),
       body: Column(
         children: [
           TextField(),
@@ -43,15 +51,13 @@ class NewPostState extends State<NewPost> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    WidgetBottomAppbar(widgetIcon: Icons.camera_alt, text: "ถ่ายภาพ", page: Object()),
-                    WidgetBottomAppbar(widgetIcon: Icons.add_photo_alternate_outlined, text: "เพิ่มรูปภาพ", page: Object()), // เปลี่ยน Object เป็น Page นั้นๆในภายหลัง
-                    WidgetBottomAppbar(widgetIcon: Icons.emoji_emotions_outlined, text: "ความรู้สึก", page: Object()),
-                  ],
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  WidgetBottomAppbar(widgetIcon: Icons.camera_alt, text: "ถ่ายภาพ", page: Object()),
+                  WidgetBottomAppbar(widgetIcon: Icons.add_photo_alternate_outlined, text: "เพิ่มรูปภาพ", page: Object()), // เปลี่ยน Object เป็น Page นั้นๆในภายหลัง
+                  WidgetBottomAppbar(widgetIcon: Icons.emoji_emotions_outlined, text: "ความรู้สึก", page: Object()),
+                ],
               ),
             ],
           ),

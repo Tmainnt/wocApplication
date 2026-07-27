3acd8cf5ef9d9ee51b419aef45d841c3afa5b247
Tracy Magz <gamerthann@gmail.com>
2026-07-27 11:55:36 +0700

    insert const while return

--- lib/theme/widget_color.dart
+++ lib/theme/widget_color.dart
@@ -2,21 +2,21 @@ import "package:flutter/material.dart";
 
 class WidgetColor {
 
-  const Color textfieldShadow() => Color.fromARGB(255, 158, 158, 158);
+  Color textfieldShadow() => const Color.fromARGB(255, 158, 158, 158);
 
-  const Color widgetShadow() => Color.fromARGB(255, 158, 158, 158);
+  Color widgetShadow() => const Color.fromARGB(255, 158, 158, 158);
 
-  const Color elevatedButtonAuth() => Color.fromARGB(255, 124, 139, 253);
+  Color elevatedButtonAuth() => const Color.fromARGB(255, 124, 139, 253);
 
-  const Color dividerColor() => Color.fromARGB(255, 158, 158, 158);
+  Color dividerColor() => const Color.fromARGB(255, 158, 158, 158);
 
-  const Color cancel() => Colors.red;
+  Color cancel() =>Colors.red;
 
-  const List<Color> topNavbar() => [
+  List<Color> topNavbar() => [
     Color.fromARGB(255, 5, 30, 141),
     Color.fromARGB(255, 105, 58, 233),
     Color.fromARGB(255, 105, 3, 95),
   ];
 
-   const Color iconWithBlackBackground() => Color.fromARGB(255, 158, 158, 158);
+  Color iconWithBlackBackground() => Color.fromARGB(255, 158, 158, 158);
 }

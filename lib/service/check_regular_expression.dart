8db5f8dd3635527b9ce5b65f6d9044114541bab0
Tracy Magz <gamerthann@gmail.com>
2026-08-06 15:06:56 +0700

    delete check username regex

--- lib/service/check_regular_expression.dart
+++ lib/service/check_regular_expression.dart
@@ -4,10 +4,6 @@ class CheckRegularExpression {
     final regEx = RegExp("");
     return true;
   }
-  
-  bool checkUsername() {
-    return true;
-  }
 
   bool checkPasswrod(){
     return true;

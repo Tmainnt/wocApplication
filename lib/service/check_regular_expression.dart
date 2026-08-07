f1ea704ee0dad0063b22c5ad6d0a9f4dfe61eb95
Tracy Magz <gamerthann@gmail.com>
2026-08-07 14:07:42 +0700

    change to singleton to normal constructor

--- lib/service/check_regular_expression.dart
+++ lib/service/check_regular_expression.dart
@@ -1,6 +1,6 @@
 class CheckRegularExpression {
 
-  CheckRegularExpression._();
+  const CheckRegularExpression();
   
   bool checkEmail(String email) {
     final regEx = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+/.[a-zA-Z]{2,}$');

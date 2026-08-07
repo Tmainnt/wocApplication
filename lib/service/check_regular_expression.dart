3fee30b4626254d1cc33343ec7bc02cd6e6b6c8e
Tracy Magz <gamerthann@gmail.com>
2026-08-07 13:24:06 +0700

    remove singleton, because this object class isn't called very often

--- lib/service/check_regular_expression.dart
+++ lib/service/check_regular_expression.dart
@@ -1,9 +1,6 @@
 class CheckRegularExpression {
 
   CheckRegularExpression._();
-  static final CheckRegularExpression  _instance = CheckRegularExpression._();
-
-  CheckRegularExpression get getInstance => _instance;
   
   bool checkEmail(String email) {
     final regEx = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+/.[a-zA-Z]{2,}$');

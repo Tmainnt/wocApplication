2a09feab5c449354bd7b008f23488dd745e15475
Tracy Magz <gamerthann@gmail.com>
2026-08-25 09:36:34 +0700

    move loginActionBotton method from loginForm to this class, complete controller

--- lib/controller/authentication/login_controller.dart
+++ lib/controller/authentication/login_controller.dart
@@ -1,3 +1,45 @@
-class LoginController {
-  const LoginController();
+import 'package:flutter/widgets.dart';
+import 'package:woc/provider/user_provider.dart';
+import 'package:woc/service/auth_service.dart';
+
+class LoginController extends ChangeNotifier {
+
+  bool validEmail = true;
+  bool validPassword = true;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final UserProvider _userProvider;
+  LoginController(this._userProvider);
+
+  Future<bool> loginButtonAction() async {
+    if (validInput()) {
+      try {
+        final userData = await AuthService().loginResponseStatusCode(
+          emailController.text,
+          passwordController.text,
+        );
+        _userProvider.setUser(userData);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+    
+    notifyListeners();
+    return false;
+  }
+
+  bool validInput() {
+    if (emailController.text.isEmpty) {
+      validEmail = false;
+    }
+
+    if (passwordController.text.isEmpty) {
+      validPassword = false;
+    }
+    return true;
+  }
+
+
+  
 }

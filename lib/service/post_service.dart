9c3b270b368154c946e681dc51fe652df86799d8
Tracy Magz <gamerthann@gmail.com>
2026-09-05 14:20:51 +0700

    fix bug import path

--- lib/service/post_service.dart
+++ lib/service/post_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:woc/model/post.dart';
+import 'package:woc/model/community/post.dart';
 import 'package:http/http.dart' as http;
 
 class PostService {

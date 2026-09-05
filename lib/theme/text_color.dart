fa4757fcb03bba6f8adf098e2791da388ba51614
Tracy Magz <gamerthann@gmail.com>
2026-09-05 14:14:39 +0700

    copy some code from previous project to this project

--- lib/theme/text_color.dart
+++ lib/theme/text_color.dart
@@ -3,4 +3,13 @@ import "package:flutter/material.dart";
 class TextColor {
   Color highlightTextAuth() => Colors.orange;
   Color subText() => Color.fromARGB(255, 158, 158, 158);
+
+  Color confirmText() => Colors.blueAccent;
+  Color errorColor() => Color.fromARGB(255, 255, 22, 22);
+  Color textDark() => Color.fromARGB(255, 90, 90, 100);
+  Color profilePageSubTitleLightColor() => Color.fromARGB(255, 158, 158, 158);
+  Color profilePageSubTitleDarkColor() => Color.fromARGB(255, 90, 90, 100);
+
+
+  
 }
